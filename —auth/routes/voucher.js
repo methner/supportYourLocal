9f1cda6c8535f118879c6ba4bd20cefecbeed333
Ineke Methner <ineke.methner@icloud.com>
2020-11-25 17:11:55 +0100

    add methods to routes

--- —auth/routes/voucher.js
+++ —auth/routes/voucher.js
@@ -5,23 +5,27 @@ const router     = express.Router();
 
 
 
-router.get('/voucher', (req,res) => {              
+router.get('/', (req,res) => {              
    console.log("this is the voucher route");
    res.render('business/voucher-details');                
 });
 
 
-router.post('/', (req, res, next) => {
+router.post('/voucher', (req, res, next) => {
     const { title, description, price } = req.body;
     Voucher.create({ title, description, price })
         .then(() => {
-            res.redirect('voucher-details');
+            res.redirect('/voucher-details');
         })
         .catch(err => {
             console.log(err)
         });
 });
 
+router.get("/voucher-details", (req,res) =>  {
+    Voucher.findOne({voucher})
+    res.render("business/voucher-details", {voucher})
+})
 
 module.exports = router;
 
