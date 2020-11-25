52dbeb067ae80f7d94fd569aa3618b37a04f3df8
Ineke Methner <ineke.methner@icloud.com>
2020-11-25 14:35:59 +0100

    add voucher-details view

--- —auth/routes/voucher.js
+++ —auth/routes/voucher.js
@@ -1,14 +1,29 @@
 const express    = require("express");
+const Voucher = require("../models/Voucher");
 const router     = express.Router();
 const Business  = require('../models/Voucher');
 
 
 
 
-router.get('/', (req,res) => {            
-   // res.render('business/voucher-details', {voucher});   
-   console.log("this is the voucher route")               
+router.get('/', (req,res) => {              
+   console.log("this is the voucher route");
+   // res.render('business/voucher-details', {voucher});                
+});
+
+
+router.post('/', (req, res, next) => {
+    const { title, description, price } = req.body;
+    Voucher.create({ title, description, price })
+        .then(() => {
+            res.redirect('voucher-details');
+        });
+        .catch(err => {
+            next(err);
+        });
 });
 
 
 module.exports = router;
+
+
