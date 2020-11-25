c143527adeb21c8673d8510a8a486c65756fd58c
fdietz <felipefierrod@gmail.com>
2020-11-25 15:28:30 +0100

    company list/details

--- —auth/routes/user.js
+++ —auth/routes/user.js
@@ -1,7 +1,9 @@
 const express    = require("express");
 const router     = express.Router();
  const bcrypt    = require('bcrypt');
+ const mongoose = require('mongoose');
  const User      = require('../models/User');
+const Business   = require('../models/Business');
 
 router.get('/index', (req,res) => {            //when call the /signup
     res.render('user/index');                       //render hbs 'signup'
@@ -13,7 +15,7 @@ router.post('/signup-user', (req,res,next) => {
     const { username, password } = req.body;
 
     //  USER FORMAT CONDITIONS
-    if(password.length < 5) res.render( 'signup', {message : 'must be 2 chars min'})
+    if(password.length < 2) res.render( 'signup', {message : 'must be 2 chars min'})
     if(username === '')     res.render( 'signup', {message : 'cannot be empty'})
 
     //  CREATE A DB USER AND PASSWORD+SALT
@@ -26,7 +28,6 @@ router.post('/signup-user', (req,res,next) => {
             const salt = bcrypt.genSaltSync();
             console.log(salt);
             const hash = bcrypt.hashSync(password, salt);
-
             //  CREATE THE USER AND PASSWORD IN DB
             User.create({ username : username, password : hash })                
             .then(dbUser => {
@@ -44,7 +45,6 @@ router.post('/signup-user', (req,res,next) => {
 
 router.post('/login-user', (req, res, next)=>{
     //get user and pass
-    console.log('checking');
     const { username , password} = req.body;
     //check user and pass are correct
     User.findOne({ username: username})     ///argumento pasado de body al metodo finOne
@@ -60,11 +60,24 @@ router.post('/login-user', (req, res, next)=>{
             req.session.user = found;
             res.redirect('/user/index');
         }
-            //IF THE USER NAME MATCH BUT THE PASSW IS WRONG
+            //IF THE USER NAME MATCH BUT THE PASS IS WRONG
         else{
             res.render('login', { message : 'Invalid credentials' })
         }
     });
+});
+
+
+router.get('/company-overview', (req, res) => {
+    console.log('CHECKING');
+   // res.render('company-overview', {companyList : business})
+    //EMPTY FIND() TO GET ALL THE BUSINESS FROM DB
+      Business.find()
+      .then(business => { //     //RENDER THE COMPANIES VIEW TO DISPLAY THEM
+        res.render('user/company-overview', {companyList : business});
+      }).catch(err => {
+          console.log(err);
+      })
 });
 
 module.exports = router;
